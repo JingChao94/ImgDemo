62feb5e539a2f3530943a3c6b3178c1cf7a08a3f
VictorJing <694527037@qq.com>
2020-11-02 16:14:13 +0800

    添加项目文件。

---
+++ ImgDemo/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ImgDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "替换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "查找背景色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ImgDemo.Properties.Resources._574e9258d109b3deb392a41bcebf6c81800a4c97;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(793, 383);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "选择替换色";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.LavenderBlush;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ImgDemo/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgDemo
+{
+    public partial class Form1 : Form
+    {
+        private Point point;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = pictureBox1.Image;
+            Bitmap pbitmap = new Bitmap(img);
+            pbitmap = Conver_3(pbitmap, pbitmap.Width, pbitmap.Height, pointColor.R, pointColor.G, pointColor.B, seletedColor.R, seletedColor.G, seletedColor.B);
+
+            pictureBox1.Image = pbitmap;
+        }
+
+        Color pointColor = Color.LavenderBlush;
+        Color seletedColor = Color.LavenderBlush;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image img = this.pictureBox1.Image;
+            using (Bitmap bmp = new Bitmap(img))
+            {
+                pointColor = bmp.GetPixel(this.point.X, this.point.Y);
+                pictureBox1.BackColor = pointColor;
+            }
+        }
+        #region 图片指定颜色替换成另一种颜色 Conver_3(Bitmap img, int w, int h, int R, int G, int B, int r, int g, int b)
+        /// <summary>
+        /// 指定颜色替换成另一种颜色
+        /// </summary>
+        /// <param name="img">原图</param>
+        /// <param name="w">图宽</param>
+        /// <param name="h">图高</param>
+        /// <param name="R">要被替换颜色的RGB的R</param>
+        /// <param name="G">要被替换颜色的RGB的G</param>
+        /// <param name="B">要被替换颜色的RGB的B</param>
+        /// <param name="r">替换色的RGB的R</param>
+        /// <param name="g">替换色的RGB的G</param>
+        /// <param name="b">替换色的RGB的B</param>
+        /// <returns>处理后的结果图像</returns>
+        public static Bitmap Conver_3(Bitmap img, int w, int h, int R, int G, int B, int r, int g, int b)
+        {
+            Bitmap bt = new Bitmap(ConvertTo32bpp(img));
+            Rectangle rect = new Rectangle(0, 0, w, h);
+            BitmapData bmpdata = bt.LockBits(rect, ImageLockMode.ReadWrite, bt.PixelFormat);
+            IntPtr ptr = bmpdata.Scan0;
+            int bytes = Math.Abs(bmpdata.Stride) * bt.Height;
+            byte[] rgbValues = new byte[bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            int len = rgbValues.Length;
+            byte a =100;
+            byte R1 = (byte)R;
+            byte G1 = (byte)G;
+            byte B1 = (byte)B;
+            byte r1 = (byte)r;
+            byte g1 = (byte)g;
+            byte b1 = (byte)b;
+            for (int i = 0; i < len; i += 4)
+            {
+                //Format32bppRgb是用4个字节表示一个像素,第一个字节表示RGB的B值,第一个表示为G值,第三个表示为R值,第四个表示为Alpha值
+                if (Math.Abs(rgbValues[i] - B1) < a && Math.Abs(rgbValues[i + 1] - G1) < a && Math.Abs(rgbValues[i + 2] - R1) < a)
+                {
+                    rgbValues[i] = b1;
+                    rgbValues[i + 1] = g1;
+                    rgbValues[i + 2] = r1;
+                }
+            }
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bt.UnlockBits(bmpdata);
+            return bt;
+        }
+        #endregion      
+        #region 图片背景透明化 Conver_1(Bitmap img, int w, int h)
+        public static Bitmap Conver_1(Bitmap img, int w, int h)
+        {
+            Bitmap bt = new Bitmap(ConvertTo32bpp(img));
+            Rectangle rect = new Rectangle(0, 0, bt.Width, bt.Height);
+            BitmapData bmpdata = bt.LockBits(rect, ImageLockMode.ReadWrite, bt.PixelFormat);
+            IntPtr ptr = bmpdata.Scan0;
+            int bytes = Math.Abs(bmpdata.Stride) * bt.Height;
+            byte[] rgbValues = new byte[bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            int len = rgbValues.Length;
+            byte a = (byte)40;//颜色的误差范围
+            for (int i = 0; i < len; i += 4)
+            {
+                //Format32bppRgb是用4个字节表示一个像素,第一个字节表示RGB的B值,第一个表示为G值,第三个表示为R值,第四个表示为Alpha值
+                if (Math.Abs(rgbValues[i] - rgbValues[0]) < a && Math.Abs(rgbValues[i + 1] - rgbValues[1]) < a && Math.Abs(rgbValues[i + 2] - rgbValues[2]) < a)
+                {
+                    rgbValues[i + 3] = (byte)0;
+                }
+            }
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bt.UnlockBits(bmpdata);
+            return bt;
+        }
+        #endregion
+
+        #region 图片指定颜色透明化 Conver_2(Bitmap img, int w, int h, int R, int G, int B)
+        /// <summary>
+        /// 指定颜色透明化
+        /// </summary>
+        /// <param name="img">原图</param>
+        /// <param name="w">原图宽度</param>
+        /// <param name="h">原图高度</param>
+        /// <param name="R">指定颜色RGB的R值</param>
+        /// <param name="G">指定颜色RGB的G值</param>
+        /// <param name="B">指定颜色RGB的B值</param>
+        /// <returns>处理后的结果图像</returns>
+        public static Bitmap Conver_2(Bitmap img, int w, int h, int R, int G, int B)
+        {
+
+            //测试结果不成功,图片没有变化
+            Bitmap bt = new Bitmap(ConvertTo32bpp(img));
+            Rectangle rect = new Rectangle(0, 0, w, h);
+            BitmapData bmpdata = bt.LockBits(rect, ImageLockMode.ReadWrite, bt.PixelFormat);
+            IntPtr ptr = bmpdata.Scan0;
+            int bytes = Math.Abs(bmpdata.Stride) * bt.Height;
+            byte[] rgbValues = new byte[bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            int len = rgbValues.Length;
+            byte a = 40;
+            byte R1 = (byte)R;
+            byte G1 = (byte)G;
+            byte B1 = (byte)B;
+            for (int i = 0; i < len; i += 4)
+            {
+                //Format32bppRgb是用4个字节表示一个像素,第一个字节表示RGB的B值,第二个表示为G值,第三个表示为R值,第四个表示为Alpha值
+                if (Math.Abs(rgbValues[i] - B1) < a && Math.Abs(rgbValues[i + 1] - G1) < a && Math.Abs(rgbValues[i + 2] - R1) < a)
+                {
+                    rgbValues[i + 3] = (byte)0;
+                }
+            }
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bt.UnlockBits(bmpdata);
+            return bt;
+        }
+        #endregion
+
+        public static Bitmap ConvertTo32bpp(Image img)
+        {
+            var bmp = new Bitmap(img.Width, img.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            using (var gr = Graphics.FromImage(bmp))
+                gr.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height));
+            return bmp;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.point.X = e.X;
+            this.point.Y = e.Y;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog() == DialogResult.OK)
+            {
+                seletedColor = color.Color;
+            }
+        }
+    }
+
+
+    internal class MouseKeyHook
+    {
+        internal delegate void MouseMoveHandler(object sender, MouseEventArgs e);
+        internal event MouseMoveHandler MouseMoveEvent;
+
+        internal delegate void MouseClickHandler(object sender, MouseEventArgs e);
+        internal event MouseClickHandler MouseClickEvent;
+
+        internal event KeyEventHandler DirectionKeyDown;
+
+        private WinAPI.boardProc MoveBoardHookProcedure;
+        private WinAPI.boardProc KeyBoardHookProcedure;
+
+        private int keyHookStatus = 0;
+        private int moveHookStatus = 0;
+
+        /// <summary>
+        /// 安装钩子
+        /// </summary>
+        internal void Hook_Start()
+        {
+            if (this.keyHookStatus == 0)
+            {
+                this.KeyBoardHookProcedure = new WinAPI.boardProc(this.KeyHookProc);
+                this.keyHookStatus = WinAPI.SetWindowsHookEx(WinAPI.WH_KEYBOARD_LL, this.KeyBoardHookProcedure, WinAPI.GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName), 0);
+            }
+            if (this.moveHookStatus == 0)
+            {
+                this.MoveBoardHookProcedure = new WinAPI.boardProc(this.MouseHookProc);
+                this.moveHookStatus = WinAPI.SetWindowsHookEx(WinAPI.WH_MOUSE_LL, this.MoveBoardHookProcedure, IntPtr.Zero, 0);
+            }
+        }
+
+        /// <summary>
+        /// 取消钩子
+        /// </summary>
+        internal void Hook_Clear()
+        {
+            if (this.keyHookStatus != 0)
+            {
+                WinAPI.UnhookWindowsHookEx(this.keyHookStatus);
+                this.keyHookStatus = 0;
+            }
+            if (this.moveHookStatus != 0)
+            {
+                WinAPI.UnhookWindowsHookEx(this.moveHookStatus);
+                this.moveHookStatus = 0;
+            }
+        }
+
+        /// <summary>
+        /// 鼠标处理事件
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private int MouseHookProc(int nCode, int wParam, IntPtr lParam)
+        {
+            WinAPI.MouseHookStruct MyMouseHookStruct = (WinAPI.MouseHookStruct)Marshal.PtrToStructure(lParam, typeof(WinAPI.MouseHookStruct));
+            switch (wParam)
+            {
+                case WinAPI.WM_MOUSEMOVE:
+                    {
+                        this.MouseMoveEvent(this, new MouseEventArgs(MouseButtons.None, 0, MyMouseHookStruct.pt.X, MyMouseHookStruct.pt.Y, 0));
+                        break;
+                    }
+                case WinAPI.WM_RBUTTONDOWN:
+                    {
+                        var e = new MouseEventArgs(MouseButtons.Right, 1, MyMouseHookStruct.pt.X, MyMouseHookStruct.pt.Y, 0);
+                        this.MouseClickEvent(this, e);
+                        return 1;
+                    }
+
+            }
+            return WinAPI.CallNextHookEx(this.moveHookStatus, nCode, wParam, lParam);
+        }
+
+        /// <summary>
+        /// 键盘处理事件
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private int KeyHookProc(int nCode, int wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                WinAPI.KeyHookStruct kbh = (WinAPI.KeyHookStruct)Marshal.PtrToStructure(lParam, typeof(WinAPI.KeyHookStruct));//获取钩子的相关信息
+                KeyEventArgs e = new KeyEventArgs((Keys)(kbh.vkCode));//获取KeyEventArgs事件的相磁信息
+                switch (e.KeyCode)
+                {
+                    case Keys.RButton | Keys.MButton | Keys.Space:
+                    case Keys.Back | Keys.Space:
+                    case Keys.LButton | Keys.MButton | Keys.Space:
+                    case Keys.LButton | Keys.RButton | Keys.MButton | Keys.Space:
+                        {
+                            if (kbh.flags == 1)
+                            {
+                                // 这里写按下后做什么事 
+                                this.DirectionKeyDown(this, e);
+                            }
+                            return 1;
+                        }
+                }
+            }
+            return WinAPI.CallNextHookEx(this.keyHookStatus, nCode, wParam, lParam);
+        }
+
+    }
+
+
+    public static class WinAPI
+    {
+        internal delegate int boardProc(int nCode, int wParam, IntPtr lParam);//钩子拦截处理函数
+
+        internal const int WH_KEYBOARD_LL = 13;//键盘事件
+
+        internal const int SW_SHOWNOACTIVATE = 4;//显示和激活窗体
+        internal const int SW_HIDE = 0;//应灿窗体
+
+        internal const int WH_MOUSE_LL = 14;//鼠标事件
+        internal const int WM_MOUSEMOVE = 0x200;
+        internal const int WM_LBUTTONDOWN = 0x201;
+        internal const int WM_RBUTTONDOWN = 0x204;
+        internal const int WM_MBUTTONDOWN = 0x207;
+        internal const int WM_LBUTTONUP = 0x202;
+        internal const int WM_RBUTTONUP = 0x205;
+        internal const int WM_MBUTTONUP = 0x208;
+        internal const int WM_LBUTTONDBLCLK = 0x203;
+        internal const int WM_RBUTTONDBLCLK = 0x206;
+        internal const int WM_MBUTTONDBLCLK = 0x209;
+
+        /// <summary>
+        /// 设置窗体置顶
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        internal static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// 查找窗体
+        /// </summary>
+        /// <param name="lpClassName"></param>
+        /// <param name="lpWindowName"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        internal extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        /// <summary>
+        /// 获取窗体Rectangle
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="lpRect"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        internal static extern int GetWindowRect(IntPtr hwnd, out RectangleStruct lpRect);
+
+        /// <summary>
+        /// 设置窗体状态
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="nCmdShow"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern int ShowWindow(IntPtr hWnd, uint nCmdShow);
+
+        /// <summary>
+        /// 获取光标位置
+        /// </summary>
+        /// <param name="pot"></param>
+        /// <returns></returns>
+        [DllImport("User32.dll")]
+        internal extern static bool GetCursorPos(ref PointStruct pot);
+
+        /// <summary>
+        /// 设置光标位置
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        [DllImport("User32.dll")]
+        internal extern static void SetCursorPos(int x, int y);
+
+        /// <summary>
+        /// 安装钩子
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <param name="lpfn"></param>
+        /// <param name="hInstance"></param>
+        /// <param name="threadId"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        internal static extern int SetWindowsHookEx(int idHook, boardProc lpfn, IntPtr hInstance, int threadId);
+
+        /// <summary>
+        /// 卸载钩子
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        internal static extern bool UnhookWindowsHookEx(int idHook);
+
+        /// <summary>
+        /// 调用下一个钩子
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        internal static extern int CallNextHookEx(int idHook, int nCode, int wParam, IntPtr lParam);
+
+        /// <summary>
+        /// 获取模块句柄
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr GetModuleHandle(string name);
+
+        /// <summary>
+        /// 矩形坐标
+        /// </summary>
+        internal struct RectangleStruct
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        /// <summary>
+        /// 坐标
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct PointStruct
+        {
+            public int X;
+            public int Y;
+        }
+
+        /// <summary>
+        /// 鼠标钩子返回信息结构体
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal class MouseHookStruct
+        {
+            public PointStruct pt;
+            public int hwnd;
+            public int wHitTestCode;
+            public int dwExtraInfo;
+        }
+
+        /// <summary>
+        /// 键盘钩子返回信息结构体
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal class KeyHookStruct
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+    }
+}
